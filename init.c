c1a5d51255f4a91601c54098bd0531635b108c66
Mariana Pihur <mpihur@c2r3s9.42london.com>
2024-08-24 18:26:00 +0100

    init safe upd

--- init.c
+++ init.c
@@ -23,21 +23,18 @@ void	init(t_table *table, int i)
 	table->forks = safe_malloc(table->philo_nbr * sizeof(t_fork));
 	while ((table->forks)[i])
 	{
-		//(table->forks)[i] = safe_malloc(sizeof())
-		(table->forks)[i]->fork_id = i + 1;
-		if (pthread_mutex_init((table->forks)[i++]->fork, NULL) != 0)
-			error_exit_free("Fork mutex init error\n", 1,table);
-		/*Error code above needs to be updated*/
+		(table->forks)[i]->fork_id = i;
+		handle_mtx_err(pthread_mutex_init((table->forks)[i++]->fork, NULL), INIT, table);
 	}
-	i = 0;
-	while ((table->philos)[i])
+	i = -1;
+	while ((table->philos)[++i])
 	{
 		(table->philos)[i]->id = i + 1;
 		(table->philos)[i]->meals_counter = 0;
 		(table->philos)[i]->full = false;
-		//(table->philos)[i]->last_meal_time = ?
-		(table->philos)[i]->left_fork = (i + 1) % table->philo_nbr;
-		(table->philos)[i]->right_fork = i;
+		(table->philos)[i]->table = table;
+		(table->philos)[i]->left_fork = &(table->forks)[(i + 1) % table->philo_nbr];
+		(table->philos)[i]->right_fork = &(table->forks)[i];
 		if (i == 0)
 			(table->philos)[i]->right_fork = table->philo_nbr;
 		pthread_create((table->philos)[i]->thread_id, NULL, *simulation, (void *)(table->philos)[i]);
--- safe_functions.c
+++ safe_functions.c
@@ -12,6 +12,44 @@ void	*safe_malloc(size_t bytes, t_table *table)
 	return (ret);
 }
 
+void	handle_mtx_err(int status, t_opcode opcode, t_table *table)
+{
+	if (status == 0)
+		return ;
+	if (status == EINVAL && (opcode == LOCK || opcode == UNLOCK))
+		error_exit_free("The mutex value is not valid\n", table);
+	else if (status == EINVAL && opcode == INIT)
+		error_exit_free("The attr value is invalid\n", 1, table);
+	else if (status == EDEADLK)
+		error_exit_free("A deadlock will occur if the thread is blocked waiting for mtx\n", 1, table);
+	else if (status == EPERM)
+		error_exit_free("Current thread doesn't hold a lock on mutex\n");
+	else if (status == ENOMEM)
+		error_exit_free("The process can't alloc enough memory for mtx creation\n");
+	else if (status == EBUSY)
+		error_exit_free("Mutex is locked");
+}
+
+void	handle_thrd_err(int status, t_opcode opcode, t_table *table)
+{
+	if (status == 0)
+		return ;
+	if (status == EAGAIN)
+		error_exit_free("No resources for extra thread\n", 1, table);
+	else if (status == EPERM)
+		error_exit_free("The caller doesn't have the permission\n", 1, table);
+	else if (status == EINVAL && opcode == CREATE)
+		error_exit_free("The value of attr is invalid\n", 1, table);
+	else if (status == EINVAL && (opcode == JOIN || opcode == DETACH))
+		error_exit_free("The value specified is not joinable\n", 1, table);
+	else if (status == ESRCH)
+		error_exit_free("No thread couble be found corresponding to the one"
+		"specified by the given thread ID, thread", 1, table);
+	else if (status == EDEADLK)
+		error_exit_free("A deadlock was detected\n", 1, table);
+}
+
+/*
 void	safe_mutex_handle(pthread_mutex_t *mutex, t_opcode opcode)
 {
 	if (opcode == LOCK)
@@ -24,4 +62,4 @@ void	safe_mutex_handle(pthread_mutex_t *mutex, t_opcode opcode)
 		pthread_mutex_destroy(mutex);
 	else
 		error_exit("Wrong opcode for mutex handle\n", 1);
-}
+}*/
