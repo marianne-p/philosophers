bcc5829e621ce198d4c7bee54cc99f7a3ea008aa
Mariana Pihur <mpihur@c3r5s1.42london.com>
2024-09-04 14:49:46 +0100

    branch

--- init.c
+++ init.c
@@ -16,27 +16,27 @@
 *`void * (*start_routine)` pointer to function which takes `void *`
 */
 
-void	init(t_table *table, int i)
+void	init(t_cafe *cafe, int i)
 {
-	table->end_simulation = false;
-	table->philos = safe_malloc(table->philo_nbr * sizeof(t_philo));
-	table->forks = safe_malloc(table->philo_nbr * sizeof(t_fork));
-	while ((table->forks)[i])
+	cafe->end_simulation = false;
+	cafe->philos = safe_malloc(cafe->philo_nbr * sizeof(t_philo), cafe);
+	cafe->straws = safe_malloc(cafe->philo_nbr * sizeof(t_straw), cafe);
+	while ((cafe->straws)[i])
 	{
-		(table->forks)[i]->fork_id = i;
-		handle_mtx_err(pthread_mutex_init((table->forks)[i++]->fork, NULL), INIT, table);
+		(cafe->straws)[i]->straw_id = i;
+		handle_mtx_err(pthread_mutex_init((cafe->straws)[i++]->straw, NULL), INIT, cafe);
 	}
 	i = -1;
-	while ((table->philos)[++i])
+	while ((cafe->philos)[++i])
 	{
-		(table->philos)[i]->id = i + 1;
-		(table->philos)[i]->meals_counter = 0;
-		(table->philos)[i]->full = false;
-		(table->philos)[i]->table = table;
-		(table->philos)[i]->left_fork = &(table->forks)[(i + 1) % table->philo_nbr];
-		(table->philos)[i]->right_fork = &(table->forks)[i];
+		(cafe->philos)[i]->id = i + 1;
+		(cafe->philos)[i]->drinks_counter = 0;
+		(cafe->philos)[i]->full = false;
+		(cafe->philos)[i]->cafe = cafe;
+		(cafe->philos)[i]->left_straw = &(cafe->straws)[(i + 1) % cafe->philo_nbr];
+		(cafe->philos)[i]->right_straw = &(cafe->straws)[i];
 		if (i == 0)
-			(table->philos)[i]->right_fork = table->philo_nbr;
-		pthread_create((table->philos)[i]->thread_id, NULL, *simulation, (void *)(table->philos)[i]);
+			(cafe->philos)[i]->right_straw = cafe->philo_nbr;
+		pthread_create((cafe->philos)[i]->thread_id, NULL, *simulation, (void *)(cafe->philos)[i]);
 	}
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: mpihur <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:37:50 by mpihur            #+#    #+#             */
-/*   Updated: 2024/08/20 16:12:13 by mpihur           ###   ########.fr       */
+/*   Updated: 2024/09/04 13:34:35 by mpihur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,43 +19,48 @@
 #include <sys/time.h>
 #include <limits.h>
 #include <stdbool.h>
+#include <errno.h>
 
 enum e_optcode {
-	LOCK = 1,
-	UNLOCK = 2,
-	INIT = 3,
-	DESTROY = 4
+	LOCK,
+	UNLOCK,
+	INIT,
+	DESTROY,
+	CREATE,
+	JOIN,
+	DETACH
 } t_opcode;
 
-typedef struct s_fork
+typedef struct s_straw
 {
-	pthread_mutex_t	*fork;
-	int				fork_id;
-}	t_fork;
+	pthread_mutex_t	*straw;
+	int				straw_id;
+}	t_straw;
 
 typedef struct s_philo
 {
 	int			id;
-	long		meals_counter;
+	long		drinks_counter;
 	bool		full;
-	long		last_meal_time;
-	t_fork		*left_fork;
-	t_fork		*right_fork;
+	long		last_drink_time;
+	t_cafe		*cafe;
+	t_straw		*left_straw;
+	t_straw		*right_straw;
 	pthread_t	thread_id;
 }	t_philo;
 
-typedef struct s_table
+typedef struct s_cafe
 {
 	long	philo_nbr;
 	long	time_to_die;
 	long	time_to_eat;
 	long	time_to_sleep;
-	long	nbr_limit_meals;
+	long	nbr_limit_drinks;
 	long	start_simulation;
 	long	end_simulation;
-	t_fork	**forks;
+	t_straw	**straws;
 	t_philo	**philos;
-}	t_table;
+}	t_cafe;
 
 /*libft.c*/
 size_t		ft_strlen(const char *str);
@@ -64,6 +69,11 @@ long long	ft_atoi(const char *nptr);
 /*main.c*/
 bool	check_is_int(int argc, char **argv, int i);
 void	error_exit(char *str, int r);
-void	parse_philo(int argc, char **argv, t_table *table);
+void	parse_philo(int argc, char **argv, t_cafe *table);
+
+/*safe functions*/
+void	*safe_malloc(size_t bytes, t_cafe *table);
+void	handle_mtx_err(int status, t_opcode opcode, t_cafe *table);
+void	handle_thrd_err(int status, t_opcode opcode, t_cafe *table);
 
 #endif
